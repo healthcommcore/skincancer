2ca834b75e21fe74c381fedeb2ce5c82f7fedb92
hccadmin <dave_rothfarb@dfci.harvard.edu>
2014-07-21 16:52:30 -0400

    Preliminary stages of definitions functionality and box styling. Need to switch to toggle and embed div into page.tpl.php

--- sites/all/themes/ask_theme/js/defs.js
+++ sites/all/themes/ask_theme/js/defs.js
@@ -4,22 +4,52 @@
 	 carcinoma: 'A type of cancer that starts in cells that make up the skin or the tissue-lining organs',
 	 chemotherapy: 'The use of medication (chemicals) to treat disease',
 	 immunotherapy: 'Treating a disease by inducing, enhancing, or suppressing an immune response',
-	 sun-exposure: 'Sun exposure you get in small ways every day without realizing it that adds up to more UV exposure',
+	 sun_exposure: 'Sun exposure you get in small ways every day without realizing it that adds up to more UV exposure',
 	 tanning: 'Using a tanning bed, booth, or sunlamp to get tan (does not include spray tanning)',
 	 melanoma: 'A serious form of skin cancer, treatable in early stages, can be fatal when advanced',
 	 moles: 'A common brown spot or growth on the skin that can be either flat or raised, and is generally round and regularly shaped',
-	 non-melanoma: 'Skin cancer that forms in the lower part of the epidermis (the outer layer of the skin) or in squamous cells, but not in melanocytes (skin cells that make pigment).',
+	 non_melanoma: 'Skin cancer that forms in the lower part of the epidermis (the outer layer of the skin) or in squamous cells, but not in melanocytes (skin cells that make pigment).',
 	 scc: 'Squamous cell carcinoma (non-melanoma skin cancer), another common form of skin cancer, not usually life-threatening.',
 	 skincancer: 'The uncontrolled growth of abnormal skin cells.',
-	 skin-self-check: 'Thoroughly checking all of your skin to prevent untreated skin cancer',
+	 skin_self_check: 'Thoroughly checking all of your skin to prevent untreated skin cancer',
 	 spf: 'Sun Protection Factor, the relative amount of sunburn protection that a sunscreen can provide an average user when used correctly',
-	 uv: 'Ultraviolet rays that you get from the sun and tanning beds'
+	 uv_rays: 'Ultraviolet rays that you get from the sun and tanning beds'
   }
 
  $(document).ready(function() {
 	 var terms = $('.def');
-	 terms.on('click', function (e) {
+	 terms.on('click mouseover', function (e) {
 		 e.preventDefault();
+		 var id = e.target.id;
+		 id = id.split('-').join('_');
+		 var match = findMatch(Object.keys(defs), id);
+		 var defBox = contain(match);
+		 $(e.target).parent().prepend(defBox);
+		 $(defBox).toggle();
+		 //console.log(match);
+
+	 })
+	 .on('mouseout', function (e) {
+		 $('#defBox').remove();
 	 });
+
+	 // findMatch()
+	 var findMatch = function(keys, id) {
+	   var match;
+		 keys.forEach( function(term) {
+			 if(term == id) {
+			   //console.log(defs[term]);
+			   match = defs[term];
+			 }
+		 });
+		 return match;
+	 }
+
+	 var contain = function(def) {
+		 var container = '<div id="defBox" class="def-container"><p>';
+		 container += def;
+		 container += '</p></div>';
+		 return container;
+	 }
  });
 })(jQuery);
