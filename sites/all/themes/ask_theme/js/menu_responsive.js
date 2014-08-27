6111349a9f380eda1801e43f6cfd18b8739b7c71
hccadmin <dave_rothfarb@dfci.harvard.edu>
2014-08-27 17:04:37 -0400

    Responsive menu in good shape! Need to clean up style a bit but functionality is working!

--- sites/all/themes/ask_theme/js/menu_responsive.js
+++ sites/all/themes/ask_theme/js/menu_responsive.js
@@ -2,28 +2,42 @@
 	$(document).ready(function() {
 		var WINDOW_SIZE = 992;
 		var submenus = $('#main-menu-collapse .expanded ul');
-		var dropdown = '<a href="#" class="dropdown-toggle submenu-reveal" data-toggle="dropdown">></a>';
+		var dropdown = '<a href="#" class="dropdown-toggle submenu-reveal" data-toggle="dropdown"></a>';
 
-		var respond = function () {
+		respond();
+		$(window).resize(respond);
+
+		/*
+		*/
+
+
+
+		function respond () {
 			if(window.innerWidth < WINDOW_SIZE) {
-				$(submenus).each(function () {
-					$(this).addClass('dropdown-menu').parent().prepend(dropdown);
+				if( !($(submenus[0]).hasClass('dropdown-menu')) && !(existy(submenus[0])) ) {
+					$(submenus).each(function () {
+						$(this).addClass('dropdown-menu').parent().prepend(dropdown);
+					});
+				}
+				$('.submenu-reveal').on('click', function() {
+					$(this).toggleClass('subnav-on');
 				});
 			}
 			else {
 				$(submenus).each(function () {
-					$(this).removeClass('dropdown-menu');
-					/*
-					if( $('#main-menu-collapse').find('.submenu-reveal') ) {
-						$('.submenu-reveal').remove();
-					}
-					*/
+					$(this).removeClass('dropdown-menu').prevAll('.dropdown-toggle').remove();
 				});
 			}
 		}
 
-		respond();
-		$(window).resize(respond);
+		function existy(submenu) {
+			if ($(submenu).find('dropdown-toggle').length > 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
 
 	});
 })(jQuery);
