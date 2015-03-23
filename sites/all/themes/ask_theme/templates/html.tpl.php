37d2f9d58e79f8427c4118863c1ba699724c2820
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-03-23 15:18:56 -0400

    Added Google Analytics code

--- sites/all/themes/ask_theme/templates/html.tpl.php
+++ sites/all/themes/ask_theme/templates/html.tpl.php
@@ -6,6 +6,16 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-60978411-1', 'auto’);
+		ga('require', 'displayfeatures’);
+		ga('require', 'linkid', 'linkid.js');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
