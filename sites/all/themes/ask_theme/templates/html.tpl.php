ec3e6a8a3b3037d55f3ac6c7a93847117f7b5f84
hccadmin <dave_rothfarb@dfci.harvard.edu>
2015-04-02 09:48:29 -0400

    Updates to Google Analytics code

--- sites/all/themes/ask_theme/templates/html.tpl.php
+++ sites/all/themes/ask_theme/templates/html.tpl.php
@@ -11,8 +11,17 @@
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-60978411-1', 'auto’);
-		ga('require', 'displayfeatures’);
+
+		<?php
+			if(user_is_logged_in()) {
+				$gacode = "ga('create', 'UA-60978411-1', {'userid' : '%s'});";
+				echo sprintf($gacode, $user->uid);
+			}
+			else {
+				echo "ga('create', 'UA-60978411-1', 'auto');";
+			}
+		?>
+		ga('require', 'displayfeatures');
 		ga('require', 'linkid', 'linkid.js');
 		ga('send', 'pageview');
 	</script>
